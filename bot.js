17ac92ce52d32ab36e129d4e4bd7fd3e65c2f494
alpha4002 <51008152+alpha4002@users.noreply.github.com>
2019-05-25 14:54:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "$"
+var prefix = "-"
 var adminprefix = '$'
 
 
@@ -23,7 +23,7 @@ client.on("message", message => {
 //bc online
 
 
-  var prefix = "$";
+  var prefix = "_";
 
   client.on("message", message => {
   
@@ -66,23 +66,23 @@ m.sendMessage(args)
 });
 
   client.on('message', msg => {
-    if(msg.content === '$help')
+    if(msg.content === '-help')
     msg.reply('Check Your DM :white_check_mark:')
   });
   
   
   client.on("message", message => {
-    if (message.content === "$help") {
+    if (message.content === "-help") {
      const embed = new Discord.RichEmbed() 
          .setColor("#00FF00")
          .setThumbnail(message.author.avatarURL)
          .setDescription(`**Help|هيلب
 
-       $obc | لأرسال برود كاست للكل
+       -obc | لأرسال برود كاست للكل
 
-       $bc  |  لأرسال برود كاست للأونلاين
+       -bc  |  لأرسال برود كاست للأونلاين
 
-       $adminbc | برودكاست عادي
+       -adminbc | برودكاست عادي
 
        ** `)
    message.author.sendEmbed(embed)
